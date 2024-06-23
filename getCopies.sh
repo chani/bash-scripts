1c224bf704fe3b098b88ae8d272ceeb5e8c03afb
chani <himself@jeanbruenn.info>
2024-06-24 01:29:23 +0200

    getCopies: version 2

--- getCopies.sh
+++ getCopies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #####################################################################
 ##                                                                 ##
-##     getCopies v0.1                                              ##
+##     getCopies v0.2                                              ##
 ##     this script can be used to find copies of files             ##
 ##                                                                 ##
 #####################################################################
@@ -9,111 +9,121 @@
 #####################################################################
 ##                                                                 ##
 ##    Author:    Jean-Michel Bruenn <himself@jeanbruenn.info>      ##
-##    Copyright: 2008-2018                                         ##
+##    Copyright: 2008-2024                                         ##
 ##    License:   MIT (see LICENSE file)                            ##
 ##                                                                 ##
 #####################################################################
 
-# Possible values are sha1sum and md5sum
-C_METHOD="md5sum";
-# where to search for data
-C_DIRECTORY="$1";
-# where to move copies (will automatically create directory "copies")
-W_DIR="$2";
+# Checksum command. 
+# e.g. md5sum, sha1sum, cksum -a sha224, rhash --md4 --simple
+C_CHECKSUM="sha1sum"
 
-# ----------------------------------------------------------------- #
-# No need to touch anything below this line                         #
-# ----------------------------------------------------------------- #
+# Where to search for duplicates
+P_SRC="$1"
+# Where to move duplicates to
+P_DST="$2/copies"
+
+# how many bytes to read from start end end of file for partial
+# checksum (to speed up processing). Will not read from end if
+# smaller than this*2. Smaller values do not necessarily make
+# it faster - because e.g. a value of 20 will cause more files
+# to be checked with a full hash (which then is slow).
+S_PART=1024
+
+# The follwoing three files would be best on tmpfs
+F_CHECKSUMS="/tmp/getCopies.txt"
+F_SIZES="/tmp/getCopies.sizes.txt"
+F_PARTIAL="/tmp/getCopies.partial.txt"
 
 cnv_filename() {
-  # get ending from file (ending)
-  ENDING=$(echo "$@" | sed -r 's|.*/||' | sed -r 's|.*\.||');
-  # get name from file (filename)
-  BEFORE=$(echo "$@" | sed -r 's|.*/||' | sed -r 's|\..*||');
+  local fullname=$(basename -- "$@")
+  local extension="${fullname##*.}"
+  local filename="${fullname%.*}"
+  local i=1
+  local re='^[0-9]+$'
 
-  if [ -f "$W_DIR"/copies/"$BEFORE"."$ENDING" ] ; then
-    # file exists let's add a number to the filename
-    # and check whether that file exists too (we set
-    # this number higher until we found a not existing
-    # filename.
-    echo "+ Copy detected" >&2
-
-    for (( I=1; $I <= 999; I++ )) ; do
-      if [ ! -f "$W_DIR"/copies/"$BEFORE"."$I"."$ENDING" ] ; then
-        # wow, we got a free filename. Let's break the
-        # for and give back the new filename
-        D_NAME="$BEFORE"."$I"."$ENDING";
-        break;
-      fi
+  if [ -f "$P_DST"/"$fullname" ]; then
+    if [[ $extension =~ $re ]]; then 
+      i=$extension
+    fi
+    D_NAME="$filename"."$i";
+    while [ -f "$P_DST"/"$D_NAME" ]; do
+      ((i=i+1))
+      D_NAME="$filename"."$i";
+      sleep 0.01
     done
-    echo "+ Renamed to: $BEFORE.$I.$ENDING" >&2
-  else
-    # the filename is free already.
-    D_NAME="$BEFORE"."$ENDING";
+    echo "+ Renamed to: $D_NAME" >&2
   fi
 }
 
-echo "+ getting file list" >&2
-W_FILES=$(find "$C_DIRECTORY" -type f);
-
-if [ -f /tmp/getCopies.txt ] ; then
-  rm -rf /tmp/getCopies.txt
-  echo "+ deleted /tmp/getCopies.txt" >&2
+if [ ! -d "${P_DST}" ]; then
+  mkdir -p "${P_DST}"
 fi
+if [ -f "${F_CHECKSUMS}" ]; then
+  truncate -s0 "${F_CHECKSUMS}"
+fi
+touch "${F_CHECKSUMS}"
+if [ -f "${F_SIZES}" ]; then
+  truncate -s0 "${F_SIZES}";
+fi
+touch "${F_SIZES}"
+if [ -f "${F_PARTIAL}" ]; then
+  truncate -s0 "${F_PARTIAL}"
+fi
+touch "${F_PARTIAL}"
 
-echo "+ generating sums" >&2
+PROCESSED=0
+COPIES=0
 
-IFS=$'\n';
-for FILENAME in $W_FILES; do
-  if [[ "$C_METHOD" == "sha1sum" ]] ; then
-    # user wanted sha1sums
-    SUM=$(sha1sum "$FILENAME" | sed 's| .*||');
-  elif [[ "$C_METHOD" == "md5sum" ]] ; then
-    # user wanted md5sums
-    SUM=$(md5sum "$FILENAME" | sed 's| .*||');
+((D_PART=S_PART*2))
+
+while read -r FILENAME; do
+  SIZE=$(stat --printf=%s "${FILENAME}");
+  if lh=$(grep -m1 "|$SIZE" "${F_SIZES}"); then
+    echo "+ Duplicate candidate detected (By size)..."
+    
+    # each size exists only once in sizes file. So if it finds a duplicate
+    # size also it's partial hash needs to be created
+    LFILENAME=$(echo "$lh" | awk -F'|' '{print $1}');
+    LPSUM=$({ head -c $S_PART "${LFILENAME}"; if [ $SIZE -gt $D_PART ]; then tail -c $S_PART "${LFILENAME}"; fi; } | ${C_CHECKSUM} | awk '{print $1}')
+    if ! grep -q -m1 "|${LPSUM}" "${F_PARTIAL}"; then
+      echo "$LFILENAME|$LPSUM" | tee -a "${F_PARTIAL}" | xargs -0 printf "+ Added (partial checksum): %s"
+    fi
+
+    PSUM=$({ head -c $S_PART "${FILENAME}"; if [ $SIZE -gt $D_PART ]; then tail -c $S_PART "${FILENAME}"; fi; } | ${C_CHECKSUM} | awk '{print $1}')
+    if fh=$(grep -m1 "|$PSUM" "${F_PARTIAL}"); then
+      echo "+ Duplicate candidate detected (By partial checksum)..."
+
+      # each partial hash exists only once in partial file. So if it finds a
+      # duplicate partial hash it's full hash needs to be created, too
+      FFILENAME=$(echo $fh | awk -F'|' '{print $1}');
+      FFSUM=$(${C_CHECKSUM} "${FFILENAME}" | awk '{print $1}');
+      if ! grep -q -m1 "|${FFSUM}" "${F_CHECKSUMS}"; then
+        echo "$FFILENAME|$FFSUM" | tee -a "${F_CHECKSUMS}" | xargs -0 printf "+ Added ($C_CHECKSUM): %s"
+      fi
+
+      SUM=$(${C_CHECKSUM} "${FILENAME}" | awk '{print $1}');
+      if ! grep -q -m1 "|$SUM" "${F_CHECKSUMS}"; then
+        # no copy detected - add
+	echo "$FILENAME|$SUM" | tee -a "${F_CHECKSUMS}" | xargs -0 printf "+ Added ($C_CHECKSUM): %s"
+      else
+        # duplicate detected 
+        D_NAME=$(echo "$FILENAME" | awk -F '/' '{print $NF}');
+	echo "+ Duplicate detected (By $C_CHECKSUM)..." >&2
+        echo "+ Moving $D_NAME to $P_DST/$D_NAME" >&2
+        # lets make sure not to overwrite anything.
+        cnv_filename "$D_NAME"
+        # now lets move the file to our copies folder.
+        #mv "$FILENAME" "$W_DIR/copies/$D_NAME";
+        ((COPIES=COPIES+1))
+      fi
+    else
+      echo "$FILENAME|$PSUM" | tee -a "${F_PARTIAL}" | xargs -0 printf "+ Added (partial checksum): %s"
+    fi
   else
-    # sum not recognized
-    echo "You have to set C_METHOD to a correct value!";
-    exit 0
+    echo "$FILENAME|$SIZE" | tee -a "${F_SIZES}" | xargs -0 printf "+ Added (size): %s"
   fi
+  ((PROCESSED=PROCESSED+1))
+done< <(find "$P_SRC" -type f)
 
-  TOADD="$FILENAME:$SUM";
-  echo "$TOADD" >> /tmp/getCopies.txt
-  echo "+ added $TOADD" >&2
-done
-
-FILECOUNT=$(echo "$W_FILES" | wc -w);
-
-echo "+ processed $FILECOUNT files" >&2
-
-mkdir -v "$W_DIR/copies";
-
-E_FILES=$(sort -nr /tmp/getCopies.txt);
-
-IFS=$'\n';
-for LINE in $E_FILES; do
-  F_NAME=$(echo "$LINE" | sed -r 's|:.*||');
-  F_SUM=$(echo "$LINE" | sed -r 's|.*:||');
-  F_SUMS=$(grep "$F_SUM" /tmp/getCopies.txt | wc -l);
-
-  if [[ "$F_SUMS" -eq "1" ]] || [[ "$F_SUMS" -eq "0" ]] ; then
-    # no copy detected
-    continue;
-  else
-    # wow - copy detected
-    D_NAME=$(echo "$F_NAME" | sed 's|.*/||');
-    echo "+ detected $F_SUMS copies of $D_NAME" >&2
-    COPIES="$COPIES $D_NAME";
-    echo "+ moving $D_NAME to $W_DIR/copies/$D_NAME" >&2
-    # lets make sure not to overwrite anything.
-    cnv_filename "$F_NAME"
-    # now lets move the file to our copies folder.
-    mv "$F_NAME" "$W_DIR/copies/$D_NAME";
-    # now we need to remove this line from /tmp/getCopies.txt
-    # otherwise it will result in false positives.
-    sed -i 's|'$F_NAME':'$F_SUM'||' /tmp/getCopies.txt
-  fi
-done
-
-COPYCOUNT=$(echo "$COPIES" | wc -w);
-echo "+ moved $COPYCOUNT copies";
+echo "+ Moved $COPIES copies of $PROCESSED files";
